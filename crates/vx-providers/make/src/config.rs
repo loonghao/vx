9f636d41efebd5c9094708faac507db4a2df35f8
longhao <hal.long@outlook.com>
2026-01-15 00:30:59 +0800

    fix(providers): fix rust path calculation and make Windows support
    Rust:
    - Remove duplicate archive dir prefix from executable_relative_path
    - Path should be 'rustc/bin/rustc' after strip_prefix, not 'rust-{version}-{platform}/rustc/bin/rustc'
    
    Make:
    - Windows is NOT supported via vx - recommend 'just' as alternative
    - Add platform constraint to only support macOS and Linux
    - Provide clear error messages with installation alternatives

--- crates/vx-providers/make/src/config.rs
+++ crates/vx-providers/make/src/config.rs
@@ -9,24 +9,10 @@ use vx_runtime::{Os, Platform};
 pub struct MakeConfig;
 
 impl MakeConfig {
-    /// Get the executable name for the platform
-    pub fn get_executable_name(platform: &Platform) -> &'static str {
-        match platform.os {
-            Os::Windows => "make.exe",
-            _ => "make",
-        }
-    }
-
     /// Get common system paths where make might be installed
     #[allow(dead_code)]
     pub fn system_paths(platform: &Platform) -> Vec<&'static str> {
         match platform.os {
-            Os::Windows => vec![
-                "C:\\Program Files\\GnuWin32\\bin\\make.exe",
-                "C:\\ProgramData\\chocolatey\\bin\\make.exe",
-                "C:\\msys64\\usr\\bin\\make.exe",
-                "C:\\cygwin64\\bin\\make.exe",
-            ],
             Os::MacOS => vec![
                 "/usr/bin/make",
                 "/usr/local/bin/make",
@@ -42,11 +28,6 @@ impl MakeConfig {
     #[allow(dead_code)]
     pub fn install_commands(platform: &Platform) -> Vec<(&'static str, &'static str)> {
         match platform.os {
-            Os::Windows => vec![
-                ("chocolatey", "choco install make"),
-                ("winget", "winget install GnuWin32.Make"),
-                ("scoop", "scoop install make"),
-            ],
             Os::MacOS => vec![("homebrew", "brew install make")],
             Os::Linux => vec![
                 ("apt", "sudo apt install make"),
@@ -64,30 +45,8 @@ mod tests {
     use vx_runtime::Arch;
 
     #[test]
-    fn test_executable_name_windows() {
-        let platform = Platform {
-            os: Os::Windows,
-            arch: Arch::X86_64,
-        };
-        assert_eq!(MakeConfig::get_executable_name(&platform), "make.exe");
-    }
-
-    #[test]
-    fn test_executable_name_unix() {
-        let platform = Platform {
-            os: Os::Linux,
-            arch: Arch::X86_64,
-        };
-        assert_eq!(MakeConfig::get_executable_name(&platform), "make");
-    }
-
-    #[test]
-    fn test_system_paths_not_empty() {
+    fn test_system_paths_unix() {
         let platforms = vec![
-            Platform {
-                os: Os::Windows,
-                arch: Arch::X86_64,
-            },
             Platform {
                 os: Os::MacOS,
                 arch: Arch::Aarch64,
@@ -102,5 +61,15 @@ mod tests {
             let paths = MakeConfig::system_paths(&platform);
             assert!(!paths.is_empty(), "Should have system paths for {:?}", platform.os);
         }
+    }
+
+    #[test]
+    fn test_windows_not_supported() {
+        let platform = Platform {
+            os: Os::Windows,
+            arch: Arch::X86_64,
+        };
+        let paths = MakeConfig::system_paths(&platform);
+        assert!(paths.is_empty(), "Windows should have no system paths");
     }
 }
